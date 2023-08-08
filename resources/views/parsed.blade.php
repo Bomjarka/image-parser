ea5b36351a9db74cb96f29c7335d57777a748340
a.chirkin <a.chirkin@robo.finance>
2023-08-08 14:31:15 +0700

    refactoring

--- resources/views/parsed.blade.php
+++ resources/views/parsed.blade.php
@@ -5,25 +5,31 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header d-flex justify-content-between">
-                        <h5>Parsed from: {{ $parsedWebsite }}</h5>
+                        <h5>Parsed from: {{ $web->url }}</h5>
                     </div>
                     <div class="card-body">
                         <div class="card-deck">
                             <table class="table table-striped">
-                                <thead>
-                                <tr>Total images found: {{ $foundImagesCount }}, Total images parsed: {{ $parsedImagesCount }} Total size: {{ $totalSize }}</tr>
-                                </thead>
                                 <tbody>
+
                                 @foreach($parsedImages as $parsedImageGroup)
                                     <tr>
                                         @foreach($parsedImageGroup as $parsedImage)
-                                            <td><img class="img-fluid" src="{{ asset('storage/' . $parsedImage) }}" alt="{{ $parsedImage }}" size="100x100"></td>
+                                            <td><img class="img-fluid" src="{{ asset('storage/' . $web->host . '/' . $parsedImage->name) }}" alt="{{ $parsedImage->name }}" size="100x100"></td>
                                         @endforeach
                                     </tr>
                                 @endforeach
                                 </tbody>
                             </table>
+
                         </div>
+                    </div>
+                    <div class="card-footer text-muted">
+                        На странице обнаружено {{ $parsedImagesCount }} изображений на: {{ $totalSize }}
+                        <form method="POST" action="{{ route('destroy.url', ['web' => $web]) }}">
+                            @csrf
+                            <button class="btn btn-danger" type="submit">Delete</button>
+                        </form>
                     </div>
                 </div>
             </div>
